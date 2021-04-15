e27c8008f6c03aa2bbe992b4a5dd4e235626b3ab
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-04-15 18:48:17 -0400

    Changed Java Code

--- app/src/main/java/com/example/animalvoices/MainActivity.java
+++ app/src/main/java/com/example/animalvoices/MainActivity.java
@@ -12,21 +12,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button1, button2; // creating class variables
-    MediaPlayer mpCows, mpPigs;
+    MediaPlayer mpBee, mpWb;
     int playing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button) findViewById(R.id.btnCows);
-        button2 = (Button) findViewById(R.id.btnPigs); // button controls instantiated
-        button1.setOnClickListener(bCows);
-        button2.setOnClickListener(bPigs);
-        mpCows = new MediaPlayer();
-        mpCows = MediaPlayer.create(this,R.raw.beyonce);
-        mpPigs = new MediaPlayer();
-        mpPigs = MediaPlayer.create(this, R.raw.waris);
+        button1 = (Button) findViewById(R.id.btnBN);
+        button2 = (Button) findViewById(R.id.btnWB); // button controls instantiated
+        button1.setOnClickListener(bYonce);
+        button2.setOnClickListener(bWaris);
+        mpBee = new MediaPlayer();
+        mpBee = MediaPlayer.create(this,R.raw.beyonce);
+        mpWb = new MediaPlayer();
+        mpWb = MediaPlayer.create(this, R.raw.waris);
         playing = 0;
 
         // changing action bar color
@@ -35,18 +35,18 @@ public class MainActivity extends AppCompatActivity {
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#87CEEB"));
         actionBar.setBackgroundDrawable(colorDrawable);
     }
-    Button.OnClickListener bCows = new Button.OnClickListener() {
+    Button.OnClickListener bYonce = new Button.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (playing) {
                 case 0:
-                    mpCows.start();
+                    mpBee.start();
                     playing = 1;
                     button1.setText("Pause This Song");
                     button2.setVisibility(View.INVISIBLE);
                     break;
                 case 1:
-                    mpCows.pause();
+                    mpBee.pause();
                     playing = 0;
                     button1.setText("Play Beyonce Song");
                     button2.setVisibility(View.VISIBLE);
@@ -54,18 +54,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-    Button.OnClickListener bPigs = new Button.OnClickListener() {
+    Button.OnClickListener bWaris = new Button.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (playing) {
                 case 0:
-                    mpPigs.start();
+                    mpWb.start();
                     playing = 1;
                     button2.setText("Pause This Song");
                     button1.setVisibility(View.INVISIBLE);
                     break;
                 case 1:
-                    mpPigs.pause();
+                    mpWb.pause();
                     playing = 0;
                     button2.setText("Play Waris Baig Song");
                     button1.setVisibility(View.VISIBLE);
